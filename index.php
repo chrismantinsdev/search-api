d03a526be0a631ffb5727ff667634f96b990459d
chrismantinsdev <chris.mantins.dev@gmail.com>
2019-03-26 16:57:30 -0300

    Index File commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+	<title>Teste de Google Search</title>
+</head>
+<body>
+
+	<button onclick="getSearchResults()">Encontrar Resultados</button>
+
+
+	<script 
+	src="http://code.jquery.com/jquery-3.3.1.min.js"
+	integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+	crossorigin="anonymous">  
+	</script>
+	<script>
+		function getSearchResults() {
+
+			$.ajax({
+				url: 'https://www.googleapis.com/customsearch/v1?key=AIzaSyBZKYPoJHSnBGcXGXR2hfBBR_koEJs3dMw&cx=010872003141797311628:lfpyihnstgs&q=Yaslip&gl=br&gl=brasil',
+				type:'GET',
+				dataType : 'json',
+				beforeSend:function(){
+					console.log('Efetuando Busca... ')
+				}
+			})
+			.done(function(data, statusText, resObject) {
+   				var jsonData = resObject.responseJSON
+   				console.log('Dados Encontrados: \n')
+				console.log(jsonData)
+			})
+			.fail(function(e, textStatus, msg){
+				console.log('Erro: '+msg + '\n'+e+'\n'+textStatus)
+				alert("Erro!")
+			})
+		}
+
+	</script>
+
+</body>
+</html>
